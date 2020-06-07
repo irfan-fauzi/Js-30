b7962e4080e58d10713ce78c332c52360c545356
irfan-fauzi <irfanwintech@yahoo.com>
2020-06-07 14:29:47 +0700

    flex

--- drum-electric/main.js
+++ drum-electric/main.js
@@ -2,11 +2,14 @@ window.addEventListener('keydown', function (e) {
     // tangkap el audio (yang hanya berisi data keyboard 'keycode' masing" tombol)
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if (!audio) return // stop the function
+    // penting !!!
+    audio.currentTime = 0; // rewind
     audio.play();
+    // tangkap elemen dengan class key
     const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
     key.classList.add('pukul')
     setTimeout(function () {
         key.classList.remove('pukul');
-    }, 200);
+    }, 100);
 
 });
